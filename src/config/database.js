a706e6bdc00f036247a6bc145e4955d4e62955e2
yashbitcode <yashkumar.sre.177@gmail.com>
2025-10-29 18:40:18 +0530

    edits

--- src/config/database.js
+++
@@ -1,8 +0,0 @@
-require("dotenv/config");
-const mongoose = require("mongoose");
-
-const connect = async () => {
-    await mongoose.connect(process.env.MONGO_URI);
-};
-
-module.exports = connect;
